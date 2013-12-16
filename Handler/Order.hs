c3eb45e9ad084040ad2eb9f2725cf7b5496d1192
Kentaro Maeda <many.front@gmail.com>
2013-12-16 19:14:08 +0900

    とりあえずできた。
    課題 - 面倒なので明細1件ごとにPOSTを投げているかつ、連絡先情報は完全に捨てているので、対応が必要。

--- Handler/Order.hs
+++ Handler/Order.hs
@@ -3,7 +3,16 @@ module Handler.Order where
 import Import
 
 getOrderR :: Handler Html
-getOrderR = error "Not yet implemented: getOrderR"
+getOrderR = do
+    -- Get the list of articles inside the database.
+    -- 以下は全て、テンプレートに与えるパラメータ(product, productWidget, enctype,,,)
+    orders <- runDB $ selectList [] [Asc OrderId]
+    defaultLayout $ do
+        $(widgetFile "order")
 
-postOrderR :: Handler Html
-postOrderR = error "Not yet implemented: postOrderR"
+postOrderR :: Handler Value
+postOrderR = do
+  order <- parseJsonBody_
+  $(logInfo) "parse OK"
+  tid <- runDB $ insert (order :: Order)
+  jsonToRepJson $ object ["status" .= ("ok" :: Text)]
